a6deac8dd75c45e1b5e2d7fd4332a8d0456394d6
VeraMarty <vera.martemyanova12@gmail.com>
2026-03-10 17:44:26 +0500

    3

--- index.js
+++ index.js
@@ -18,40 +18,12 @@ function addListeners() {
             const block = document.getElementById('scaleBlock');
             animaster().scale(block, 1000, 1.25);
         });
+    document.getElementById('fadeOutPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeOutBlock');
+            animaster().fadeOut(block, 5000);
+        });
 }
-
-// /**
-//  * Блок плавно появляется из прозрачного.
-//  * @param element — HTMLElement, который надо анимировать
-//  * @param duration — Продолжительность анимации в миллисекундах
-//  */
-// function fadeIn(element, duration) {
-//     element.style.transitionDuration =  `${duration}ms`;
-//     element.classList.remove('hide');
-//     element.classList.add('show');
-// }
-//
-// /**
-//  * Функция, передвигающая элемент
-//  * @param element — HTMLElement, который надо анимировать
-//  * @param duration — Продолжительность анимации в миллисекундах
-//  * @param translation — объект с полями x и y, обозначающими смещение блока
-//  */
-// function move(element, duration, translation) {
-//     element.style.transitionDuration = `${duration}ms`;
-//     element.style.transform = getTransform(translation, null);
-// }
-//
-// /**
-//  * Функция, увеличивающая/уменьшающая элемент
-//  * @param element — HTMLElement, который надо анимировать
-//  * @param duration — Продолжительность анимации в миллисекундах
-//  * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
-//  */
-// function scale(element, duration, ratio) {
-//     element.style.transitionDuration =  `${duration}ms`;
-//     element.style.transform = getTransform(null, ratio);
-// }
 
 function getTransform(translation, ratio) {
     const result = [];
@@ -70,14 +42,19 @@ function animaster() {
             element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(translation, null);
         },
-        scale(element, duration, translation){
+        scale(element, duration, ratio){
             element.style.transitionDuration =  `${duration}ms`;
             element.style.transform = getTransform(null, ratio);
         },
-        fadeIn(element, duration, translation){
+        fadeIn(element, duration){
             element.style.transitionDuration =  `${duration}ms`;
             element.classList.remove('hide');
             element.classList.add('show');
+        },
+        fadeOut(element, duration){
+            element.style.transitionDuration =  `${duration}ms`;
+            element.classList.remove('show');
+            element.classList.add('hide');
         }
     }
 }
